304565df139b9bbf4e5e697512ca339e64ec9d5f
Kwon Yeongkyeong <86418308+asaei623@users.noreply.github.com>
2024-08-26 20:52:43 +0900

    계단_오르기.py 2번째 풀이 추가

--- DP/계단_오르기.py
+++ DP/계단_오르기.py
@@ -33,3 +33,42 @@ for i in range(1, n+1):
 
 # 출력
 print(dp(n, s))
+
+------------------------------
+# 2번째 풀이
+# '밟지 않는 계단'의 최소값을 구하여 푼다.
+# d 테이블을 1차원 리스트로 만들어 풀 수 있다.
+def dp(n, s):
+    # 1. 테이블 정의하기
+    # d[i] : i번째까지 올라섰을 때 밟지 않은 계단들의 합의 최소(단, i번째 계단은 밟지 않는다.)
+    d = [0] * (n+4)
+    
+    # 2. 점화식 찾기
+    # d[k] = s[k] + min(d[k-2], d[k-3])
+    
+    # 3. 초기값 정하기
+    d[1] = s[1]
+    d[2] = s[2]
+    d[3] = s[3]
+
+    # 예외처리 : n이 3 이하인 경우, 바로 리턴
+    if n <= 3:
+        return d[n]
+    
+    for i in range(4, n+1):
+        d[i] = s[i] + min(d[i-2], d[i-3])
+        
+    return d[n]
+
+# 입력
+n = int(input())
+s = []
+s.append(0)
+for _ in range(n):
+    s.append(int(input()))
+
+# 출력
+if n <= 2:
+    print(sum(s))
+else:
+    print(sum(s) - min(dp(n-1, s), dp(n-2, s)))
